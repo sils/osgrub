43169d458a449910c90065cfad38bd9d80c8a762
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@090431d0-f2c8-0082-fb3c-f583aebb5382>
2013-04-17 09:01:53 +0000

    Added upper case keyboard layout for us.

--- kernel/interrupts/keyboard/keyboardInt.c
+++ kernel/interrupts/keyboard/keyboardInt.c
@@ -11,9 +11,48 @@ void keyboardHandler(registers_t regs)
 	  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
 		0,			/* 29   - Control */
 	  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
-	 '\'', '`',   1,		/* Left shift */
+	 '\'', '\'',   1,		/* Left shift */
 	 '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
 	  'm', ',', '.', '/',   1,				/* Right shift */
+	  '*',
+		0,	/* Alt */
+	  ' ',	/* Space bar */
+		0,	/* Caps lock */
+		0,	/* 59 - F1 key ... > */
+		0,   0,   0,   0,   0,   0,   0,   0,
+		0,	/* < ... F10 */
+		0,	/* 69 - Num lock*/
+		0,	/* Scroll Lock */
+		0,	/* Home key */
+		0,	/* Up Arrow */
+		0,	/* Page Up */
+	  '-',
+		0,	/* Left Arrow */
+		0,
+		0,	/* Right Arrow */
+	  '+',
+		0,	/* 79 - End key*/
+		0,	/* Down Arrow */
+		0,	/* Page Down */
+		0,	/* Insert Key */
+		0,	/* Delete Key */
+		0,   0,   0,
+		0,	/* F11 Key */
+		0,	/* F12 Key */
+		0,	/* All other keys are undefined */
+	};
+	unsigned char kbd_us_up[128] =
+	{
+		0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
+	  '(', ')', '_', '+', '\b',	/* Backspace */
+	  '\t',			/* Tab */
+	  'Q', 'W', 'E', 'R',	/* 19 */
+	  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',	/* Enter key */
+		0,			/* 29   - Control */
+	  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 39 */
+	 '|', '"',   1,		/* Left shift */
+	 '|', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+	  'M', '<', '>', '?',   1,				/* Right shift */
 	  '*',
 		0,	/* Alt */
 	  ' ',	/* Space bar */
@@ -63,7 +102,7 @@ void keyboardHandler(registers_t regs)
 				break;
 			default:
 				if(ctrlKeys & 0x01)
-					putChar(upperCase(kbd_us[scancode]));
+					putChar(kbd_us_up[scancode]);
 				else
 					putChar(kbd_us[scancode]);
 				break;
