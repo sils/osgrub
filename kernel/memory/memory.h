37e8c41d36fedad643102389717a7fcaac272a82
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@090431d0-f2c8-0082-fb3c-f583aebb5382>
2013-04-19 08:21:04 +0000

    minor changes

--- kernel/memory/memory.h
+++ kernel/memory/memory.h
@@ -1,7 +1,13 @@
 #ifndef memory_h
 #define memory_h
 
-#include "../stdio/stdio.h"
+#include "../include.h"
+
+typedef struct allocatetMem
+{
+	char *startAdress;
+	unsigned int count;
+} allocatedMem_t;
 
 unsigned char *memcpy(unsigned char *, const unsigned char *, u32int);
 unsigned char *memset(unsigned char *, const unsigned char, u32int);
