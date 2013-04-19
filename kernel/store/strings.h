93bc5346a61289523ba439365b8336251e7855b9
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@090431d0-f2c8-0082-fb3c-f583aebb5382>
2013-04-19 22:23:34 +0000

    Got the memorymanager running.

--- kernel/store/strings.h
+++ kernel/store/strings.h
@@ -7,6 +7,8 @@ void strcat(char *, char *);
 void strcpy(char *, char *);
 u32int strlen(char *);
 char strcmp(char *, char *);
+int strtoi(char *, unsigned char);
+unsigned int strtoui(char *, unsigned char);
 
 #define upperCase(val) val-'a'+'A'
 #define lowerCase(val) val-'A'+'a'
